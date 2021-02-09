7eeeef11d2be60760cb6cd37a505adbaf5757395
Hunter Laine <hunterglaine@gmail.com>
2021-02-08 17:48:14 -0800

    Create Book class in model.py

--- model.py
+++ model.py
@@ -5,6 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class Book(db.Model):
+    """Book class."""
+
+    __tablename__ = 'books'
+
+    isbn = db.Column(db.String(13), primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text)
+    page_length = db.Column(db.Integer, nullable=False)
+    image_thumb = db.Column(db.String)
+    image_large = db.Column(db.String)
+
+
+    def __repr__(self):
+
+        return f'<Book ISBN={self.isbn} title={self.title}>'
+
 
 def connect_to_db(flask_app, db_name, echo=True):
     """Connect to database."""
