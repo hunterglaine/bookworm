6184dd1e90373c60ad9ab496e6f06aff7a3be6b4
Hunter Laine <hunterglaine@gmail.com>
2021-02-09 18:35:48 -0800

    Start creation of homepage

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,11 +1,22 @@
 "use strict";
 
-function Test() {
+// Create and dsiplay the navigation bar at the top of the page - logo, search bar, create account button, and log in butto
+function TopNavigation() {
     return (
-        <div>Testing that things are connecting alright.</div>
+        <React.Fragment>
+            <img src="/static/img/bookworm_logo.jpg" alt="Bookworm Logo" id="logo"></img>
+            <form for="book-search" class ="topnav" id="book-search">
+                <input  type="text" placeholder="Search for book by title or author"></input>
+                <input type="submit"></input>
+            </form>
+            <button class ="topnav" id="create-account">Create Account</button>
+            <button class ="topnav" id="log-in">Log In</button>
+        </React.Fragment>
     );
 }
 
+
+
 ReactDOM.render(
-    <Test />, document.querySelector('#root')
+    <TopNavigation />, document.querySelector('#root')
 );
