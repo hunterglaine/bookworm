dad7d69df8b5dbc825a4c9e96ccad5602f2c8c3d
Hunter Laine <hunterglaine@gmail.com>
2021-02-09 21:08:48 -0800

    Work on Event Listener

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,16 +1,34 @@
 "use strict";
 
+// import "secrets.sh";
+
 // Create and dsiplay the navigation bar at the top of the page - logo, search bar, create account button, and log in butto
 function TopNavigation() {
+    function bookSearch(evt) {
+        evt.preventDefault();
+        console.log("Clicked")
+    }
+    // React.useEffect(() => {
+    //     document.addEventListener('click', (evt) => {
+    //         // const bookQuery = document.getElementById('book-search').value.split().join("+")
+
+    //         fetch(`https://www.googleapis.com/books/v1/volumes?q=harry+potter&maxResults=10&key=`)
+    //         .then (response => response.json())
+    //         .then(data => console.log(data))
+    
+    //     });
+    // }, []);
+    
+    
     return (
         <React.Fragment>
             <img src="/static/img/bookworm_logo.jpg" alt="Bookworm Logo" id="logo"></img>
-            <form for="book-search" class ="topnav" id="book-search">
+            <form id="book-search">
                 <input  type="text" placeholder="Search for book by title or author"></input>
-                <input type="submit"></input>
+                <input type="submit" onClick={bookSearch}></input>
             </form>
-            <button class ="topnav" id="create-account">Create Account</button>
-            <button class ="topnav" id="log-in">Log In</button>
+            <button id="create-account" >Create Account</button>
+            <button id="log-in">Log In</button>
         </React.Fragment>
     );
 }
