089c178a111624414ea2f9d981463204c562b971
Hunter Laine <hunterglaine@gmail.com>
2021-02-09 23:08:30 -0800

    Fix create_book function in crud.py and add connection to db

--- crud.py
+++ crud.py
@@ -2,7 +2,7 @@
 
 from model import db, Book, User, UserBook, Category, UserBookCategory, Event, EventBook, UserEvent, Friendship, connect_to_db
 
-def create_book(isbn, title, author, description, page_length, image)
+def create_book(isbn, title, author, description, page_length, image):
     """Create and return a new book"""
 
     book = Book(isbn=isbn, title=title, author=author, description=description,
@@ -12,4 +12,9 @@ def create_book(isbn, title, author, description, page_length, image)
     db.session.commit()
 
     return book
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
 
