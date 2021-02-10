483d8378c229397dec1b759ffbe00dab9e1d3b4a
Hunter Laine <hunterglaine@gmail.com>
2021-02-09 16:04:31 -0800

    Create basic connection between HTML and JSX

---
+++ static/js/homepage.jsx
@@ -0,0 +1,11 @@
+"use strict";
+
+function Test() {
+    return (
+        <div>Testing that things are connecting alright.</div>
+    );
+}
+
+ReactDOM.render(
+    <Test />, document.querySelector('#root')
+);
