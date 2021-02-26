e0451b9e914c1cf16eec1db3be2edcf5be518ca2
Hunter Laine <hunterglaine@gmail.com>
2021-02-25 22:55:16 -0800

    Update all events

--- static/components/AllEvents.jsx
+++ static/components/AllEvents.jsx
@@ -30,13 +30,13 @@ function AllEvents(props) {
             <h1>Book Club Meetings</h1>
             {allEvents.events ? allEvents.events.map(event => 
                     (<div id={event.id}>
-                        <h2>There is a book club on {event.event_date}!</h2>
+                        <h2>There is a book club on {event.event_date.slice(0,16)}!</h2>
                         <h3>Location: {event.city} </h3>
                         <h3>Attendees</h3>
                         {event.attending.map(attendee => 
-                            (<p value={`${attendee.first_name} ${attendee.first_name}`} >{`${attendee.first_name} ${attendee.first_name}`}</p>))
+                            (<p value={`${attendee.first_name} ${attendee.last_name}`} >{`${attendee.first_name} ${attendee.last_name}`}</p>))
                         }
-                        {props.userLoggedIn.userId ? <input type="button" value="Attend" onClick={addAttendee} /> : <Link to="/create-account">Create and account or log in to attend an event</Link>}
+                        {props.userLoggedIn.userId ? <input type="button" value="Attend" onClick={addAttendee} /> : <Link to="/create-account">Create an account or log in to attend an event</Link>}
                     </div>)) : ''
                 }
         </div>
